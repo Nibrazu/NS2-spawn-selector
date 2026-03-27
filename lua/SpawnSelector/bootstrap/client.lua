7cf663f5365e63f90ed30f39b7e736d41724b4f4
Nibrazu <nritter310@gmail.com>
2026-03-27 16:35:18 -0300

    Fix UI script path for spawn selection menu (remove lua/ prefix)

--- lua/SpawnSelector/bootstrap/client.lua
+++ lua/SpawnSelector/bootstrap/client.lua
@@ -1,4 +1,4 @@
 -- Spawn Selector client bootstrap
 
 Script.Load("lua/SpawnSelector/bootstrap/shared.lua")
-AddClientUIScriptForClass("AlienCommander", "lua/SpawnSelector/GUI/GUISpawnSelectionMenu")
+AddClientUIScriptForClass("AlienCommander", "SpawnSelector/GUI/GUISpawnSelectionMenu")
